b69befce44b57ed7b1e5089b6850c6c25affc6ba
github username <scott.gloyna01@gmail.com>
2021-02-08 20:53:04 -0600

    commit tree 8b412f5293a00a71ba0b798335beb06ac7d40674

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+Comment.destroy_all
+User.destroy_all
+
+# post1 = Post.create(title: "Feeling Groovy", content: "I'm feeling so groovy")
+# post2 = Post.create(title: "So Awesome", content: "Man am I a great blogger OR WHAT!")
+# cool = Category.create(name: "Cool")
+# post1.categories.build(name: "Cool")
+
+# user1 = User.create(username: 'RealBigFish')
+# user2 = User.create(username: 'CoolGuy5')
+
+# comment1 = Comment.create(content: "And another thing, how come there aren't any vegetabls at the movie theater...", user: user1, post: post1)
+# post1.comments.build(content: "Great Post!", user: user2)
+
+    
