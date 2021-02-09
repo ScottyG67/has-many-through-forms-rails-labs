dbdc1645fc2ee63750079f09947bbd255a4301b7
github username <scott.gloyna01@gmail.com>
2021-02-08 20:01:58 -0600

    error in post comment fields

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def uniq_comment_usernames
+    #list usernames that have commented, duplicated removed
+    self.users.uniq
+  end
+
+  def comment_attributes=(comment_attributes)
+    comment_attributes.values.each do |comment_attribute|
+      comment = Comment.find_or_create_by(comment_attribute)
+      self.comments << comment
+    end
+  end
+
 
 end
